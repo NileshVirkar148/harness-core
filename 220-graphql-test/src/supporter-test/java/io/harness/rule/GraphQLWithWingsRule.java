c42640f1cb30366d27dc3169851519965f488532
NileshVirkar148 <75842704+NileshVirkar148@users.noreply.github.com>
2022-01-21 15:23:35 +0530

    Update GraphQLWithWingsRule.java

--- 220-graphql-test/src/supporter-test/java/io/harness/rule/GraphQLWithWingsRule.java
+++ 220-graphql-test/src/supporter-test/java/io/harness/rule/GraphQLWithWingsRule.java
@@ -15,7 +15,7 @@ import com.google.inject.Module;
 import java.lang.annotation.Annotation;
 import java.util.List;
 
-public class GraphQLWithWingsRule extends WingsRule { 
+public class GraphQLWithWingsRule extends WingsRule {  
   @Override
   public List<Module> modules(List<Annotation> annotations) throws Exception {
     List<Module> modules = super.modules(annotations);
